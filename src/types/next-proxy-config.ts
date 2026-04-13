234407d67e25ffcbd7896f1336ac2206af345436
Victor Cassiano <jose_victor@live.com>
2026-04-13 13:12:20 -0300

    feat: add public-only access type

--- src/types/next-proxy-config.ts
+++ src/types/next-proxy-config.ts
@@ -9,6 +9,7 @@ export type NextProxyConfig = {
   routes: Record<string, RouteRule>;
   redirects: {
     unauthenticated: string;
+    authenticated: string;
   };
   auth: AuthConfig;
   output?: {
